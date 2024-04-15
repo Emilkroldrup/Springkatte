9b4b3491c1e19b0d1dc5e56c6c1e5eb813323441
Emilkr <Emilkroldrup@outlook.com>
2024-04-15 12:25:59 +0200

    initial project

---
+++ src/test/java/com/example/springkatte/SpringkatteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkatte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringkatteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
