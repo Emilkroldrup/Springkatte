f7c4799e3e9d6e117f82db6a3047e1f403e5918b
Mads505m <73273757+Mads505m@users.noreply.github.com>
2024-04-21 16:12:11 +0200

    UserTestCreate

---
+++ src/test/java/com/example/springkatte/pets/domain/PetsDAOTest.java
@@ -0,0 +1,7 @@
+package com.example.springkatte.pets.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PetsDAOTest {
+
+}
---
+++ src/test/java/com/example/springkatte/users/domain/UserDAOTest.java
@@ -0,0 +1,7 @@
+package com.example.springkatte.users.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDAOTest {
+
+}
