7dab8cbfaa6a672e6c5fc933717478a92136b834
Mads505m <73273757+Mads505m@users.noreply.github.com>
2024-04-23 00:15:17 +0200

    Lorte pis login system til security config, så kan man login

--- src/main/java/com/example/springkatte/config/DatabaseConfig.java
+++ src/main/java/com/example/springkatte/config/DatabaseConfig.java
@@ -1,11 +1,11 @@
 package com.example.springkatte.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
@@ -14,7 +14,7 @@ public class DatabaseConfig {
     public DataSource dataSource() {
     DriverManagerDataSource dataSource = new DriverManagerDataSource();
     dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-    dataSource.setUrl("jdbc:mysql://localhost:3306/kattehjem");
+    dataSource.setUrl("jdbc:mysql://localhost:3306/cathome");
     dataSource.setUsername("root");
     dataSource.setPassword("password");
     return dataSource;
